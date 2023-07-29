43120a312fe764d85a30668374524854d20a227e
DenisRegan <denismartinoregan@hotmail.com>
2023-07-29 16:26:13 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  console.log("PORT: "+ process.env.REACT_APP_PORT )
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Devops Final Project</h1>
+      <h1>CI/CD pipeline</h1>
     </div>
   );
 }
