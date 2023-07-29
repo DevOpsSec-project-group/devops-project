34887c61bae09f146610d74f8e7edfeecf08b6ad
DenisRegan <denismartinoregan@hotmail.com>
2023-07-29 21:50:18 +0100

    debug automated deployment

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,17 @@ sudo npm install -g pm2
 #stop any instance of the application running
 #pm2 stop devops-project
 
-
-
-
-cd devops-project
-sudo echo "devops-project"
+#cd devops-project
+#sudo echo "devops-project"
 #install dependencies
-sudo echo "********npm install"
-sudo npm install
-sudo echo  finished npm install
+#sudo echo "********npm install"
+#sudo npm install
+#sudo echo  finished npm install
 #create a production ready build of react application
 #sudo echo '********           production build    ******************** '
 #sudo npm run build
-sudo echo '***************   end production build  *****************'
+#sudo echo '***************   end production build  *****************'
 #start application
 #sudo echo 'start application'
 #sudo pm2 serve build 3001 --name "devopssec-project"
+echo "successful deployment"
